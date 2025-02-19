41f7181565af57a17035dd084948429bac867420
ElsmrSoufiane <soufianeasmar0@gmail.com>
2025-02-19 22:00:18 +0100

    tt

--- conn.php
+++ conn.php
@@ -1,7 +1,8 @@
 
-<?php
+<?php 
+echo "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>";
 try{                                              
-    $db=new PDO("mysql:host=localhost;dbname=p3","root","");
+    $db=new PDO("mysql:host=sql202.infinityfree.com;dbname=p3","if0_38354511","Gwks2JMbHEYIjS2");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
     echo "<div class='alert-primary'>".$e->getMessage()."</div>";
